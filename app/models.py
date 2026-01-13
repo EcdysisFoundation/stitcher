d51a0527eec6c483d20ca1e678c4464dd3cbfad0
Mikayla Wilson <mikaylaelectra@gmail.com>
2026-01-13 14:29:00 -0500

    create thumbnails

--- app/models.py
+++ app/models.py
@@ -442,3 +442,38 @@ def get_stats():
             "not_completed_count": not_completed_count,
         })
         return stats
+
+
+def update_one_thumbnail(guid: uuid.UUID):
+    """
+    Update one record at a time to keep previous recs to any failure.
+    """
+    # this is a temp function
+    # avoid circular imports
+    from .utils import load_resize_and_save_thumbnail
+    with Session(ENGINE) as session:
+
+        statement = select(UploadFileModel).where(UploadFileModel.guid == guid)
+        rec = session.exec(statement).first()
+        p_path = rec.panorama_path
+        thumb_path = load_resize_and_save_thumbnail(p_path, 600)
+        if thumb_path:
+            rec.panorama_thumbnail_path = str(thumb_path)
+            session.add(rec)
+            session.commit()
+
+
+def thumbnail_update():
+    """
+    Get a list of records to update.
+    """
+    # this is a temp function
+    with Session(ENGINE) as session:
+        statement = select(UploadFileModel).where(
+            UploadFileModel.panorama_thumbnail_path == None).where(
+                UploadFileModel.panorama_path.is_not(None)
+            ).options(load_only(UploadFileModel.guid))
+        results = session.exec(statement).all()
+        for r in results:
+            print(f'updating {r.guid}')
+            update_one_thumbnail(r.guid)
